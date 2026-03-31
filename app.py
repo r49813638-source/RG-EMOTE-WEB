098643580b21182ba1dff1a786fa8ee8717787e7
Rehan-source <r49813638@gmail.com>
2026-03-31 06:27:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,4 +70,5 @@ def send_emote():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render ya default 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
